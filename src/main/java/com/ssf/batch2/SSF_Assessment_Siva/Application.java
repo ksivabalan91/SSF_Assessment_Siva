e0ff7b2e06c7fb271ca168e52eaf2b1c77708a62
K.Sivabalan <ksivabalan.91@gmail.com>
2023-03-02 23:24:24 +0800

    initial commit

---
+++ src/main/java/com/ssf/batch2/SSF_Assessment_Siva/Application.java
@@ -0,0 +1,13 @@
+package com.ssf.batch2.SSF_Assessment_Siva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssf/batch2/SSF_Assessment_Siva/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssf.batch2.SSF_Assessment_Siva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
